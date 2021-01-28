3f02bde01b70f1ea84df4095540cb219c20b0e72
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-28 09:10:27 +0000

    Test stages config

--- variables_default_stages.tf
+++ variables_default_stages.tf
@@ -89,6 +89,63 @@ locals {
           name             = "Deploy"
           category         = "Deploy"
           owner            = "AWS"
+          provider         = "ECS"
+          version          = "1"
+          input_artifacts  = ["BuildArtifact"]
+          output_artifacts = []
+          configuration = {
+            ClusterName = var.custom_ecs_cluster == null ? var.name : var.custom_ecs_cluster
+            ServiceName = var.custom_ecs_service == null ? var.name : var.custom_ecs_service
+          }
+        }
+      }
+    ],
+  }
+}
+
+
+
+
+/*
+"CODECOMMIT_CODEBUILD_CLOUDFORMATION" = [
+      {
+        name = "Source"
+        action = {
+          name     = "Source"
+          category = "Source"
+          owner    = "AWS"
+          provider = "CodeCommit"
+          version  = "1"
+          configuration = {
+            BranchName           = var.defaultbranch
+            PollForSourceChanges = "false"
+            RepositoryName       = var.name
+          }
+          input_artifacts  = []
+          output_artifacts = ["SourceArtifact"]
+        }
+      },
+      {
+        name = "Build"
+        action = {
+          name             = "Build"
+          category         = "Build"
+          owner            = "AWS"
+          provider         = "CodeBuild"
+          input_artifacts  = ["SourceArtifact"]
+          output_artifacts = ["BuildArtifact"]
+          version          = "1"
+          configuration = {
+            ProjectName = element(concat(aws_codebuild_project.this.*.id, list("")), 0)
+          }
+        }
+      },
+      {
+        name = "Deploy"
+        action = {
+          name             = "Deploy"
+          category         = "Deploy"
+          owner            = "AWS"
           provider         = "CloudFormation"
           version          = "1"
           input_artifacts  = ["BuildArtifact"]
@@ -134,5 +191,5 @@ locals {
         }
       },
     ]
-  }
-}
+
+*/
