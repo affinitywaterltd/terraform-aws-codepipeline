ccdb85faca1f894dc68af1b5ad6fe9c0b49805f2
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-02-10 08:52:10 +0000

    Update policy

--- s3.tf
+++ s3.tf
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "artifacts_policy" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = [var.cross_account_role_account_princpals == "" ? local.codecommit_role_arn : var.cross_account_role_account_princpals]
+      identifiers = var.cross_account_role_account_princpals == "" ? [local.codecommit_role_arn] : var.cross_account_role_account_princpals
     }
 
     actions = [
