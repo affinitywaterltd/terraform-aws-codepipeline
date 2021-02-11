eaf4687d9140010c8c7d6ab121ea606d7901b727
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-02-11 08:33:03 +0000

    Update S3 policy

--- s3.tf
+++ s3.tf
@@ -1,29 +1,7 @@
-resource "aws_s3_bucket" "artifacts" {
-  count         = var.bucketname == "" ? 1 : 0
-  bucket        = local.bucketname
-  acl           = "private"
-  force_destroy = var.force_artifact_destroy
-
-  versioning {
-    enabled    = var.versioning
-    mfa_delete = var.mfa_delete
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = var.sse_algorithm
-      }
-    }
-  }
-
-  tags = var.tags
-}
-
 module "artifacts" {
   count  = var.bucketname == "" ? 1 : 0
   source = "github.com/affinitywaterltd/terraform-aws-s3"
-  bucket = "${local.bucketname}-test"
+  bucket = "${local.bucketname}"
   default_logging_enabled = false
 
   attach_policy = true
@@ -36,7 +14,7 @@ data "aws_iam_policy_document" "artifacts_policy" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["${local.codecommit_role_arn}"]
+      identifiers = ["${var.codecommit_role == "" ? local.codepipline_role_arn : local.codecommit_role_arn}"]
     }
 
     actions = [
@@ -47,8 +25,8 @@ data "aws_iam_policy_document" "artifacts_policy" {
     ]
 
     resources = [
-      "arn:aws:s3:::${local.bucketname}-test/*",
-      "arn:aws:s3:::${local.bucketname}-test"
+      "arn:aws:s3:::${local.bucketname}/*",
+      "arn:aws:s3:::${local.bucketname}"
     ]
   }
 }
