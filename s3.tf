8b86e0a1cca6bd8ea50855e8fb252d8baa00513a
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-02-12 08:34:22 +0000

    Update Policy

--- s3.tf
+++ s3.tf
@@ -26,6 +26,7 @@ data "aws_iam_policy_document" "artifacts_policy" {
       "arn:aws:s3:::${local.bucketname}"
     ]
   },
+  statement {
     principals {
       type        = "AWS"
       identifiers = var.codecommit_role_arn == "" ? compact(list(local.codepipeline_role_arn, "")) : compact(list(local.codepipeline_role_arn, local.codecommit_role_arn, ""))
