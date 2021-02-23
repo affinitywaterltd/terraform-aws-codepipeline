402b162c2fd07a4fa25b0627145b4f48e9da5afa
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-02-23 17:41:13 +0000

    Updated EB Application Name

--- variables_default_stages.tf
+++ variables_default_stages.tf
@@ -333,7 +333,7 @@ locals {
           region           = var.deployment_region == "" ? data.aws_region.current.name : var.deployment_region
           configuration = {
             ApplicationName = var.name
-            EnvironmentName = var.beanstalk_environemnt_name
+            EnvironmentName = "${var.name}-${var.beanstalk_environemnt_name}"
           }
         }
       }
@@ -399,7 +399,7 @@ locals {
           region           = var.deployment_region == "" ? data.aws_region.current.name : var.deployment_region
           configuration = {
             ApplicationName = var.name
-            EnvironmentName = var.beanstalk_environemnt_name
+            EnvironmentName = "${var.name}-${var.beanstalk_environemnt_name}"
           }
         }
       }
