30c82e1dbffee0a37f5235f3eae84800feb09fc4
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-03-04 09:29:20 +0000

    Add eventbus permission

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,5 +1,28 @@
+locals {
+  is_source = try(lookup(var.eventbridge_bus_config, "enabled"), false) && try(lower(lookup(var.eventbridge_bus_config, "type")), false) == "source" ? true : false
+  is_destination = try(lookup(var.eventbridge_bus_config, "enabled"), false) && try(lower(lookup(var.eventbridge_bus_config, "type")), false) == "destination" ? true : false
+}
+
+#
+# Type: Destination
+#
+
 resource "aws_cloudwatch_event_bus" "this" {
-  count = try(lookup(var.eventbridge_bus_config, "enabled"), false) && try(lower(lookup(var.eventbridge_bus_config, "type")), false) == "destination" ? 1 : 0
+  count = local.is_destination ? 1 : 0
   
   name = "eventbridge-bus-${data.aws_region.current.name}-${var.name}"
 }
+
+resource "aws_cloudwatch_event_permission" "codecommit-cross-account" {
+  count = local.is_destination && try(lookup(var.eventbridge_bus_config, "account_principal"), null) != null ? 1 : 0
+
+  principal    = try(lookup(var.eventbridge_bus_config, "account_principal"), nill)
+  action = "events:PutEvents"
+  statement_id = "codecommit-account-access-${var.name}"
+
+}
+
+
+#
+# Type: Source
+#
