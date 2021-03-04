fa00c51eeeee7695d81ff71adc530e868bf14aa0
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-03-04 08:50:33 +0000

    Update enventbus config

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,5 +1,9 @@
+locals {
+  event_bus_config = lookup(var.cross_account_config, "event_bus")
+}
+
 resource "aws_cloudwatch_event_bus" "this" {
-  try(lookup(lookup(var.cross_account_config, "event_bus"), "enabled"), false) ? 1 : 0
+  try(lookup(local.event_bus_config, "enabled"), false) ? 1 : 0
   
   name = "eventbridge-bus-${data.aws_region.current.name}-${var.name}"
 }
