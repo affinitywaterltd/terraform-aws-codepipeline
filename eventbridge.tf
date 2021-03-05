33103aa9719c9f06927a27c68e42ef9a0217db73
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-03-05 08:00:50 +0000

    Update detail

--- eventbridge.tf
+++ eventbridge.tf
@@ -77,9 +77,6 @@ resource "aws_cloudwatch_event_rule" "this_source" {
   event_pattern = <<PATTERN
 {
     "detail": {
-        "referenceType": [
-            "branch"
-        ]
     },
     "detail-type": [
         "CodeCommit Repository State Change"
