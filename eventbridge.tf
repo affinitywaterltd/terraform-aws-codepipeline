13d58582fc646aa190d11054ed067fabb6b4144f
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-03-05 07:31:58 +0000

    Update arns

--- eventbridge.tf
+++ eventbridge.tf
@@ -57,7 +57,7 @@ resource "aws_cloudwatch_event_target" "this_destination" {
   count = local.is_destination && try(lookup(var.eventbridge_bus_config, "eventbridge_arn"), null) != null ? 1 : 0
 
   arn  = try(lookup(var.eventbridge_bus_config, "eventbridge_arn"), null)
-  rule = element(concat(aws_cloudwatch_event_rule.this_destination.0.id, ""), 0)
+  rule = aws_cloudwatch_event_rule.this_destination.0.id
 }
 
 
@@ -99,6 +99,6 @@ PATTERN
 resource "aws_cloudwatch_event_target" "this_source" {
   count = local.is_source && try(lookup(var.eventbridge_bus_config, "eventbridge_arn"), null) != null ? 1 : 0
 
-  arn  = element(concat(aws_codepipeline.this.0.arn, ""), 0)
-  rule = element(concat(aws_cloudwatch_event_rule.this_source.0.id, ""), 0)
+  arn  = aws_codepipeline.this.0.arn
+  rule = aws_cloudwatch_event_rule.this_source.0.id
 }
