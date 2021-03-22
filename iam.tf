3b1608a3f1011c9656a0689bfb8590e0bce1359a
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-03-22 08:49:51 +0000

    Update syntax

--- iam.tf
+++ iam.tf
@@ -761,7 +761,7 @@ resource "aws_iam_policy" "AWSJenkinsCodePipelineUser_policy" {
         "${aws_codepipeline.this.0.arn}",
         "arn:aws:codepipeline:*:${data.aws_caller_identity.current.account_id}:actiontype:*/*/${try(lookup(var.jenkins_config, "provider"), "")}/*"
       ]
-    },
+    }
   ]
 }
 EOF
